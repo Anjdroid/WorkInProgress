93f20af240383833695248dab62d4037dc1b088f
Anja <Anja>
2017-05-23 21:23:16 +0200

    CRUD methods

--- Work_in_progress/app/src/main/java/com/example/tnuv/work_in_progress/DBHelper.java
+++ Work_in_progress/app/src/main/java/com/example/tnuv/work_in_progress/DBHelper.java
@@ -2,8 +2,17 @@ package com.example.tnuv.work_in_progress;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.Locale;
+
 
 /**
  * Created by CaptainBat on 23. 05. 2017.
@@ -66,51 +75,307 @@ public class DBHelper extends SQLiteOpenHelper {
     }
 
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        // on upgrade drop older tables
+        // on upgrade do nothing
     }
 
-    /*
- * Creating a task
- */
-    /*public long createTask(Task t, long cat_id) {
+    /**
+     * get datetime
+     * */
+    private String getDateTime() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat(
+                "yyyy-MM-dd HH:mm:ss", Locale.getDefault());
+        Date date = new Date();
+        return dateFormat.format(date);
+    }
+
+    /* METHODS FOR TABLE TASK */
+
+    /**
+    * Creating a task
+    * */
+    public long createTask(Task t, long cat_id) {
         SQLiteDatabase db = this.getWritableDatabase();
 
         ContentValues values = new ContentValues();
         values.put(KEY_NAME, t.getName());
         values.put(KEY_DESC, t.getDescription());
-        values.put(KEY_IMG, t.getImage().toString());
         values.put(KEY_VID, t.getVideo());
-        values.put(KEY_CAT_ID, cat_id);
-        values.put(KEY_CREATED, t.getCreated().toString());
+        // TODO: what to do with image??
+        values.put(KEY_IMG, t.getImage().toString());
+        // TODO: DATES?
+        values.put(KEY_CREATED, getDateTime());
         values.put(KEY_DEADLINE, t.getDeadline().toString());
 
         // insert row
-        long todo_id = db.insert(TABLE_TODO, null, values);
+        long task_id = db.insert(TABLE_TASK, null, values);
 
-        // assigning tags to todo
-        for (long tag_id : tag_ids) {
-            createTodoTag(todo_id, tag_id);
+        // assigning categories to task
+        createCategoryTask(task_id, cat_id);
+
+        return task_id;
+    }
+
+    /**
+     * get a single task
+     */
+    public Task getTask(long task_id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        String selectQuery = "SELECT  * FROM " + TABLE_TASK + " WHERE "
+                + KEY_ID + " = " + task_id;
+
+        Log.e("LOG", selectQuery);
+
+        Cursor c = db.rawQuery(selectQuery, null);
+
+        if (c != null)
+            c.moveToFirst();
+
+        Task t = new Task();
+        t.setID(c.getInt(c.getColumnIndex(KEY_ID)));
+        t.setName((c.getString(c.getColumnIndex(KEY_NAME))));
+        t.setDescription(c.getString(c.getColumnIndex(KEY_DESC)));
+        t.setVideo((c.getString(c.getColumnIndex(KEY_VID))));
+        // TODO: how to set image?
+        //t.setImage(c.getString(c.getColumnIndex(KEY_IMG)));
+        // TODO: dates?
+        //t.setCreated(c.getString(c.getColumnIndex(KEY_CREATED)));
+        //t.setDeadline(c.getString(c.getColumnIndex(KEY_DEADLINE)));
+
+        return t;
+    }
+
+    /**
+     * getting all tasks
+     * */
+    public List<Task> getAllTasks() {
+        List<Task> tasks = new ArrayList<Task>();
+        String selectQuery = "SELECT  * FROM " + TABLE_TASK;
+
+        Log.e("LOG", selectQuery);
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor c = db.rawQuery(selectQuery, null);
+
+        // looping through all rows and adding to list
+        if (c.moveToFirst()) {
+            do {
+                Task t = new Task();
+                t.setID(c.getInt(c.getColumnIndex(KEY_ID)));
+                t.setName((c.getString(c.getColumnIndex(KEY_NAME))));
+                t.setDescription(c.getString(c.getColumnIndex(KEY_DESC)));
+                t.setVideo((c.getString(c.getColumnIndex(KEY_VID))));
+                // TODO: how to set image?
+                //t.setImage(c.getString(c.getColumnIndex(KEY_IMG)));
+                // TODO: dates?
+                //t.setCreated(c.getString(c.getColumnIndex(KEY_CREATED)));
+                //t.setDeadline(c.getString(c.getColumnIndex(KEY_DEADLINE)));
+
+                tasks.add(t);
+            } while (c.moveToNext());
         }
 
-        return todo_id;
-    }*/
+        return tasks;
+    }
 
-    // add category
+    /**
+     * getting all tasks under single category name
+     * */
+    public List<Task> getAllTasksByCat(String cat_name) {
+        List<Task> tasks = new ArrayList<Task>();
 
-    // delete category
+        String selectQuery = "SELECT  * FROM " + TABLE_TASK + " td, "
+                + TABLE_CATEGORY + " tg, " + TABLE_TASK_CAT + " tt WHERE tg."
+                + KEY_NAME + " = '" + cat_name + "'" + " AND tg." + KEY_ID
+                + " = " + "tt." + KEY_CAT_ID + " AND td." + KEY_ID + " = "
+                + "tt." + KEY_TASK_ID;
 
-    // delete task
+        Log.e("LOG", selectQuery);
 
-    // update task
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor c = db.rawQuery(selectQuery, null);
 
-    // update category
+        // looping through all rows and adding to list
+        if (c.moveToFirst()) {
+            do {
+                Task t = new Task();
+                t.setID(c.getInt(c.getColumnIndex(KEY_ID)));
+                t.setName((c.getString(c.getColumnIndex(KEY_NAME))));
+                t.setDescription(c.getString(c.getColumnIndex(KEY_DESC)));
+                t.setVideo((c.getString(c.getColumnIndex(KEY_VID))));
+                // TODO: how to set image?
+                //t.setImage(c.getString(c.getColumnIndex(KEY_IMG)));
+                // TODO: dates?
+                //t.setCreated(c.getString(c.getColumnIndex(KEY_CREATED)));
+                //t.setDeadline(c.getString(c.getColumnIndex(KEY_DEADLINE)));
 
-    // get single task
+                // add to task list
+                tasks.add(t);
+            } while (c.moveToNext());
+        }
 
-    // get single category
+        return tasks;
+    }
 
-    // get all tasks
+    /**
+     * Updating a task
+     */
+    public int updateTask(Task t) {
+        SQLiteDatabase db = this.getWritableDatabase();
 
-    // get all categories
+        ContentValues values = new ContentValues();
+        values.put(KEY_NAME, t.getName());
+        values.put(KEY_DESC, t.getDescription());
+        values.put(KEY_VID, t.getVideo());
+        // TODO: what to do with image??
+        values.put(KEY_IMG, t.getImage().toString());
+        // TODO: DATES?
+        values.put(KEY_DEADLINE, t.getDeadline().toString());
+
+        // updating row
+        return db.update(TABLE_TASK, values, KEY_ID + " = ?",
+                new String[] { String.valueOf(t.getID()) });
+    }
+
+    /**
+     * Deleting a task
+     */
+    public void deleteTask(long task_id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(TABLE_TASK, KEY_ID + " = ?",
+                new String[] { String.valueOf(task_id) });
+    }
+
+    /* METHODS FOR TABLE CATEGORY */
+
+    /**
+     * Creating a category
+     */
+    public long createCategory(Category cat) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(KEY_NAME, cat.getName());
+        values.put(KEY_COL, cat.getColor());
+        values.put(KEY_LOC, cat.getLocation());
+
+        // insert row
+        long cat_id = db.insert(TABLE_CATEGORY, null, values);
+
+        return cat_id;
+    }
+
+    /**
+     * getting all categories
+     * */
+    public List<Category> getAllTags() {
+        List<Category> categories = new ArrayList<Category>();
+        String selectQuery = "SELECT  * FROM " + TABLE_CATEGORY;
+
+        Log.e("LOG", selectQuery);
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor c = db.rawQuery(selectQuery, null);
+
+        // looping through all rows and adding to list
+        if (c.moveToFirst()) {
+            do {
+                Category ca = new Category();
+                ca.setID(c.getInt((c.getColumnIndex(KEY_ID))));
+                ca.setName(c.getString(c.getColumnIndex(KEY_NAME)));
+                ca.setColor(c.getString(c.getColumnIndex(KEY_COL)));
+                ca.setLocation(c.getString(c.getColumnIndex(KEY_LOC)));
+
+                // adding to category list
+                categories.add(ca);
+            } while (c.moveToNext());
+        }
+        return categories;
+    }
+
+    /**
+     * Updating a category
+     */
+    public int updateCategory(Category cat) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(KEY_NAME, cat.getName());
+
+        // updating row
+        return db.update(TABLE_CATEGORY, values, KEY_ID + " = ?",
+                new String[] { String.valueOf(cat.getID()) });
+    }
+
+    /**
+     * Deleting a category (+ tasks under this category)
+     */
+    public void deleteCategory(Category cat, boolean should_delete_all_cat_tasks) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        // before deleting category
+        // check if tasks under this category -- should also be deleted
+        if (should_delete_all_cat_tasks) {
+            // get all tasks under this category
+            List<Task> allCatTasks = getAllTasksByCat(cat.getName());
+
+            // delete all tasks
+            for (Task t : allCatTasks) {
+                // delete task
+                deleteTask(t.getID());
+            }
+        }
+
+        // now delete the category
+        db.delete(TABLE_CATEGORY, KEY_ID + " = ?",
+                new String[] { String.valueOf(cat.getID()) });
+    }
+
+    /* METHOD FOR TABLE CATEGORY_TASK */
+
+    /*
+     * Creating category_task -> assigning a category to a task
+     */
+    public long createCategoryTask(long task_id, long cat_id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(KEY_TASK_ID, task_id);
+        values.put(KEY_CAT_ID, cat_id);
+
+        long id = db.insert(TABLE_TASK_CAT, null, values);
+
+        return id;
+    }
+
+    /**
+     * Updating a task category
+     */
+    public int updateTaskCat(long id, long cat_id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(KEY_CAT_ID, cat_id);
+
+        // updating row
+        return db.update(TABLE_TASK, values, KEY_ID + " = ?",
+                new String[] { String.valueOf(id) });
+    }
+
+    /**
+     * Deleting a task category
+     */
+    public void deleteTaskCat(long id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(TABLE_TASK, KEY_ID + " = ?",
+                new String[] { String.valueOf(id) });
+    }
+
+    // closing database
+    public void closeDB() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        if (db != null && db.isOpen())
+            db.close();
+    }
 
 }
