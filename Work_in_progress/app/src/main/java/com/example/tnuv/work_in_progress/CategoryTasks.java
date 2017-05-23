1a86e3f03077ae73779aea6eb8134da2fb723ce3
Anja <Anja>
2017-05-23 13:31:02 +0200

    Icons in zacetek CRUD

---
+++ Work_in_progress/app/src/main/java/com/example/tnuv/work_in_progress/CategoryTasks.java
@@ -0,0 +1,15 @@
+package com.example.tnuv.work_in_progress;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CategoryTasks extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_tasks);
+    }
+
+
+}
