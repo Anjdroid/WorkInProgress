8f1e3821c06d9d5af8554c1db98b0ab001e2d711
Neza Dukic <neza@Nezas-MacBook-Pro.local>
2017-05-23 12:09:50 +0200

    theme time

--- Work_in_progress/app/src/main/java/com/example/tnuv/work_in_progress/LogoActivity.java
+++ Work_in_progress/app/src/main/java/com/example/tnuv/work_in_progress/LogoActivity.java
@@ -1,5 +1,7 @@
 package com.example.tnuv.work_in_progress;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,14 @@ public class LogoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logo);
+
+        Handler mHandler = new Handler();
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(LogoActivity.this, AllTasks.class);
+                startActivity(intent);
+            }
+        }, 3000L);
     }
 }
