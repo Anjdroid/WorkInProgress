fdda80b6eee864957eb4a22d76abcb965e0c7c7b
Anja <Anja>
2017-05-30 13:39:07 +0200

    Edit task

--- Work_in_progress/app/src/main/java/com/example/tnuv/work_in_progress/EditTask.java
+++ Work_in_progress/app/src/main/java/com/example/tnuv/work_in_progress/EditTask.java
@@ -1,9 +1,15 @@
 package com.example.tnuv.work_in_progress;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,6 +17,8 @@ import java.util.List;
 public class EditTask extends AppCompatActivity {
 
     private DBHelper db;
+    private String id1;
+    private int idd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,27 +31,46 @@ public class EditTask extends AppCompatActivity {
 
         loadSpinnerData(db, spinner);
 
-        // get task id from view
-        long task_id = 1;
+        Intent intent = getIntent();
+        id1 = intent.getStringExtra("id");
+        final long task_id = Long.parseLong(id1);
         Task t = db.getTask(task_id);
 
         // set data in view as instance
+        EditText name = (EditText) findViewById(R.id.editText);
+        name.setText(t.getName());
+        EditText desc = (EditText) findViewById(R.id.editText2);
+        desc.setText(String.valueOf(t.getDescription()));
+        // TODO: get category spinner and set spinner value to task category
+        EditText video = (EditText) findViewById(R.id.editText3);
+        video.setText(t.getVideo());
+        EditText image = (EditText) findViewById(R.id.editText4);
+        image.setText(t.getImage());
+        // TODO: set date
 
-        // get updated data from view
-        String name = "";
-        String description = "";
-        String video = "";
-        String image = "";
-        String deadline = "";
+        //save button
+        Button saveButoon = (Button) findViewById(R.id.addTask);
+        saveButoon.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String name = ((EditText) findViewById(R.id.editText)).getText().toString();
+                String desc = ((EditText) findViewById(R.id.editText2)).getText().toString();
+                String video = ((EditText) findViewById(R.id.editText3)).getText().toString();
+                String image = ((EditText) findViewById(R.id.editText4)).getText().toString();
+                // TODO: deadline and category
+                Task t = db.getTask(task_id);
+                t.setName(name);
+                t.setDescription(desc);
+                t.setVideo(video);
+                t.setImage(image);
+                db.updateTask(t);
+                Task r2 = db.getTask(task_id);
+                String lala = t.getName();
+                Log.e("ime", lala);
 
-        long cat_id = 1;
-
-        Task t2 = new Task(name, description, video, image, deadline);
-
-        // update task
-        //int updateTask = db.updateTask(t2);
-        // also update category of task
-        //int update = db.updateTaskCat(task_id, cat_id);
+                Intent intent = new Intent(EditTask.this, AllTasks.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
